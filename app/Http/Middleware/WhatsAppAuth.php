da7203f62d2e836dda275332c35562289830bd64
msokaily92 <msokaily92@gmail.com>
2024-06-09 05:31:58 +0200

    A

--- app/Http/Middleware/WhatsAppAuth.php
+++ app/Http/Middleware/WhatsAppAuth.php
@@ -16,7 +16,7 @@ class WhatsAppAuth extends Middleware
                 'message' => 'Unauthrozed'
             ], 401);
         }
-        dd('Else');
+        dd('Else: '.$request->input('hub_verify_token').' == '.env('WHATSAPP_API_TOKEN'));
         return parent::handle($request, $next, ...$guards);
     }
 }
